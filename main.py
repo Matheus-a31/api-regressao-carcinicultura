e92c15f63fde6f61a890de35a39bf12c16346b51
Matheus-a31 <matheusassis3103@gmail.com>
2026-03-11 08:48:11 -0300

    feat: adiciona as depeência

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main.py
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 import os
--- schemas.py
+++ schemas.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import List
 
-from pydantic import BaseModel, Field
 
 class BiomassaRequest(BaseModel):
     peso_atual_g: float = Field(
